228c671a2d687dfb8e27fd962816472a347488bb
515hikaru <12kojima.takahiro@gmail.com>
2018-07-29 23:06:20 +0900

    AC

--- abs/q10.py
+++ abs/q10.py
@@ -1,18 +1,32 @@
 def main():
     s = input()
     s = s[::-1]
-    words = [s[::-1] for s in ['dream', 'dreamer', 'erase', 'eraser']]
+    print('len(s) =', len(s))
+    words = [ss[::-1] for ss in ['dream', 'dreamer', 'erase', 'eraser']]
+    print('words:', words)
     i = 0
-    while(i != len(s)):
+    print('s:', s)
+    c = True
+    while(True):
+        print('len(s) =', len(s))
         can = False
+        if i > len(s) - 1:
+            break
         for w in words:
-            if s[i:i+8].startswith(w):
+            print('i =', i)
+            print('s, w = ', s[i:i+len(w)], ',', w)
+            print(s[i:i+len(w)] == w)
+            if s[i:i+len(w)] == w:
                 i += len(w)
                 can = True
-        if can == False:
-            print('No')
-            return
-    print('YES')
+                break
+        if not can:
+            c = False
+            break
+    if c:
+        print('YES')
+    else:
+        print('NO')
 
 if __name__ == '__main__':
     main()
