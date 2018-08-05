5674662cba57f3c3755f30cca1e8ccac81114dc9
515hikaru <12kojima.takahiro@gmail.com>
2018-08-06 02:28:07 +0900

    C++でせいかいできるかやってみる

---
+++ abc104/c.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int c(int *p, int *q, int size, res) {}
+int main() {
+    int D, G;
+    cin >> D >> G;
+    int p[10], q[10];
+    for (int i = 0; i < D; i++) cin >> p[i] >> q[i];
+    return 0;
+}
