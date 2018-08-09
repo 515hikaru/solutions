5ed911d598655d393f2cd8fd1588b2c9b570b498
515hikaru <12kojima.takahiro@gmail.com>
2018-08-10 02:37:13 +0900

    add first problem

---
+++ book/aoj/2_5/ALDS1_1_D.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int MAX = 200000;
+    int n;
+    int a[MAX];
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int maxv = a[1] - a[0];
+    int minv = a[0];
+    for (int i = 1; i < n; i++) {
+        maxv = max(maxv, a[i] - minv);
+        minv = min(minv, a[i]);
+    }
+    cout << maxv << endl;
+    return 0;
+}
