ee12f58bd84216e984a45791089d22779fc3f869
515hikaru <12kojima.takahiro@gmail.com>
2019-12-30 02:55:36 +0900

    solve queue/dequeue

---
+++ book/aoj/04/ALDS1_3_B/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int top = 0;
+
+void solve(std::string* names, int* times, int length, int q) {
+    bool everyZero = false;
+    int totalTime = 0;
+    while (!everyZero) {
+        everyZero = true;
+        for ( int i = 0 ; i < length ; i++) {
+            if (times[i] == 0) {
+                continue;
+            } else if (times[i] <= q) {
+                totalTime += times[i];
+                times[i] = 0;
+                std::cout << names[i] << " " << totalTime << std::endl;
+                everyZero = false;
+            }  else {
+                times[i] -= q;
+                totalTime += q;
+                everyZero = false;
+            }
+        }
+    }
+}
+
+int main() {
+    const int N = 100000;
+    int length, q;
+    std::string names[N];
+    int times[N];
+    std::cin >> length >> q;
+    for (int i = 0; i < length ; i++) {
+        std::cin >> names[i] >>  times[i];
+    }
+    solve(names, times, length, q);
+    return 0;
+}
+
