f9cb7e4eafc58ee49c8ae6dbca9a77e5e298634d
515hikaru <12kojima.takahiro@gmail.com>
2019-12-30 18:36:23 +0900

    双方向連結リスト

---
+++ book/aoj/04/ALDS1_3_C/main.cpp
@@ -0,0 +1,90 @@
+#include <cstdio>
+#include <cstdlib>
+
+struct Node {
+    int key;
+    Node *prev, *next;
+};
+
+Node *nil;
+
+void init() {
+    nil = (Node *)malloc(sizeof(Node));
+    nil->next = nil;
+    nil->prev = nil;
+}
+
+void insert(int key) {
+    Node *x = (Node *)malloc(sizeof(Node));
+    x->key = key;
+    x->next = nil->next;
+    // nil->next == nil => nil->prev = x;
+    nil->next->prev = x;
+    nil->next = x;
+    x->prev = nil;
+}
+
+Node* listSearch(int key) {
+    Node *cur = nil->next;
+    while (cur !=nil && cur->key != key) {
+        cur = cur->next;
+    }
+    return cur;
+}
+
+void deleteNode(Node *t) {
+    if (t == nil) return;
+    t->prev->next = t->next;
+    t->next->prev = t->prev;
+    free(t);
+}
+
+void deleteFirst() {
+    deleteNode(nil->next);
+}
+
+void deleteLast() {
+    deleteNode(nil->prev);
+}
+
+void deleteKey(int key) {
+    deleteNode(listSearch(key));
+}
+
+void printNodes() {
+    Node *cur = nil->next;
+    int isf = 0;
+    while ( 1 ) {
+        if (cur == nil) break;
+        if(isf > 0) printf(" ");
+        isf++;
+        printf("%d", cur->key);
+        cur = cur->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    const long SIZE = 1000000;
+    init();
+    scanf("%d", &n);
+    char com[20];
+    int key;
+    for(int i = 0; i < n; i++) {
+        scanf("%s%d", com, &key);
+        if (com[0] == 'i') {
+            insert(key);
+        } else {
+            if (com[6] == 'L') {
+                deleteLast();
+            } else if (com[6] == 'F') {
+                deleteFirst();
+            } else {
+                deleteKey(key);
+            }
+        }
+    }
+    printNodes();
+    return 0;
+}
