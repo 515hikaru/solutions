dcd70a4745243cfc12917100d9bf4ebeb9edf27c
515hikaru <12kojima.takahiro@gmail.com>
2019-12-30 18:56:19 +0900

    stack

---
+++ book/aoj/04/STL/ALDS1_3_A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    stack<int> S;
+    int a, b, x;
+    string s;
+
+    while (cin >> s) {
+        if (s[0] == '*') {
+            a = S.top(); S.pop();
+            b = S.top(); S.pop();
+            x = a * b;
+            S.push(x);
+        } else if (s[0] == '+') {
+            a = S.top(); S.pop();
+            b = S.top(); S.pop();
+            x = a + b;
+            S.push(x);
+        } else if (s[0] == '-') {
+            a = S.top(); S.pop();
+            b = S.top(); S.pop();
+            x = b - a;
+            S.push(x);
+        } else {
+            S.push(atoi(s.c_str()));
+        }
+    }
+    cout << S.top() << endl;
+}
