7f67989b9f276d06d5b5146e0c7caf4241bc5fc3
515hikaru <12kojima.takahiro@gmail.com>
2020-01-01 23:41:52 +0900

    merge sort との組み合わせがうまくいかない

--- book/aoj/07/ALDS1_6_C/main.cpp
+++ book/aoj/07/ALDS1_6_C/main.cpp
@@ -2,13 +2,48 @@
 #include <string>
 using namespace std;
 
-const int N = 100000;
-int n;
+const int N = 100005;
+const long MAX = 2000000000;
 
 struct Card {
     string name;
     int number;
 };
+
+
+void merge(struct Card *A, int left, int mid, int right) {
+    cout << "boo2: left = " << left << " right = " << right << endl;
+    int n1 = mid - left;
+    int n2 = right - mid;
+    struct Card L[N / 2 + 3], R[N / 2 + 3];
+    for (int i = 0; i < n1; i++) L[i] = A[left + i];
+    for (int i = 0; i < n2; i++) R[i] = A[mid + i];
+    L[n1] = A[0]; R[n2] = A[0];
+    L[n1].number = MAX; R[n2].number = MAX;
+    int i = 0, j = 0;
+    for (int k = left; k < right; k++) {
+        if (L[i].number <= R[j].number) {
+            A[k] = L[i];
+            i++;
+        } else {
+            A[k] = R[j];
+            j++;
+        }
+    }
+}
+
+void mergeSort(struct Card *A, int left, int right) {
+    if (left + 1 < right) {
+        int mid = (left + right) / 2;
+        mergeSort(A, left, mid);
+        cout << "foo: left = " << left << " right = " << right << " mid = " << mid << endl;
+        mergeSort(A, mid, right);
+        cout << "bar: left = " << left << " right = " << right << " mid = " << mid << endl;
+        merge(A, left, mid, right);
+        cout << "boo: left = " << left << " right = " << right << " mid = " << mid << endl;
+    }
+}
+
 
 
 int partition(struct Card *A, int p, int r) {
@@ -40,10 +75,21 @@ void quickSort(struct Card *A, int left, int right) {
 }
 
 int main() {
+    int n;
     cin >> n;
-    struct Card A[N];
-    for(int i = 0; i < n; i++) cin >> A[i].name >> A[i].number;
+    struct Card A[N], B[N];
+    for(int i = 0; i < n; i++) {
+        cin >> A[i].name >> A[i].number;
+        B[i] = A[i];
+    }
+    mergeSort(B, 0, n);
     quickSort(A, 0, n-1);
+    bool flg = true;
+    for(int i = 0; i < n; i++) {
+        if (B[i].name != A[i].name) flg = false;
+    }
+    if (flg) cout << "Stable" << endl;
+    else cout << "Unstable" << endl;
     for(int i = 0; i < n; i++) {
         cout << A[i].name << " " << A[i].number << endl;
     }
